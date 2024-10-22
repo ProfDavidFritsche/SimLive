7fc7d6ca68c3639ab9824863a7cdb472acfb413c
ProfDavidFritsche <david.fritsche@hs-esslingen.de>
2024-10-22 23:16:27 +0200

    fix LineElement search

--- src/simlive/model/LineElement.java
+++ src/simlive/model/LineElement.java
@@ -218,11 +218,9 @@ public abstract class LineElement extends Element {
 		norm[0] = (B[1]-A[1])*(C[2]-A[2])-(B[2]-A[2])*(C[1]-A[1]);
 		norm[1] = (B[2]-A[2])*(C[0]-A[0])-(B[0]-A[0])*(C[2]-A[2]);
 		norm[2] = (B[0]-A[0])*(C[1]-A[1])-(B[1]-A[1])*(C[0]-A[0]);
-		if (dir[0]*norm[0]+dir[1]*norm[1]+dir[2]*norm[2] > 0.0) {
-			double[] intersect = GeomUtility.getIntersectionLinePlane(modelCoords2d, dir, A, norm);
-			if (GeomUtility.isPointInTriangle3d(A, B, C, intersect)) {
-				return intersect;
-			}
+		double[] intersect = GeomUtility.getIntersectionLinePlane(modelCoords2d, dir, A, norm);
+		if (GeomUtility.isPointInTriangle3d(A, B, C, intersect)) {
+			return intersect;
 		}
 		return null;
 	}
