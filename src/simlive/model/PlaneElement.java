887e4d9bc5329826ec0195b4b2c2224c680b39dc
ProfDavidFritsche <david.fritsche@hs-esslingen.de>
2026-09-23 11:02:22 +0200

    Update PlaneElement.java
    getGlobalFromLocalCoordinates

--- src/simlive/model/PlaneElement.java
+++ src/simlive/model/PlaneElement.java
@@ -298,7 +298,7 @@ public abstract class PlaneElement extends Element {
 		double[] coords = c0.plus(Rr.times(new Matrix(localCoords, 3))).getColumnPackedCopy();
 		if (elementNodes.length > 3) {
 			double[] n = new double[]{Rr.get(0, 2), Rr.get(1, 2), Rr.get(2, 2)};			
-			if (r < -s) {
+			if (GeomUtility.isPointInTriangle3d(nodeCoords[0], nodeCoords[1], nodeCoords[3], coords)) {
 				coords = GeomUtility.getIntersectionLinePlane(coords, n, nodeCoords[0], nodeCoords[1], nodeCoords[3]);
 			}
 			else {
